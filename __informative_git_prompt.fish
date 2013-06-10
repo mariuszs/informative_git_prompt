31a0a765e778d6c101118ccd73071ace92960ed8
Mariusz Smykuła <mariuszs@gmail.com>
2013-06-10 20:15:12 +0200

    Refactoring

--- __informative_git_prompt.fish
+++ __informative_git_prompt.fish
@@ -4,28 +4,25 @@
 # This is fish port of Informative git prompt for bash (https://github.com/magicmonty/bash-git-prompt)
 #
 
-set -gx fish_color_git_clean green
-set -gx fish_color_git_branch magenta
-set -gx fish_color_git_remote normal
+set -g fish_color_git_clean green
+set -g fish_color_git_branch magenta
+set -g fish_color_git_remote green
 
-set -g digit_sub "₁" "₂" "₃" "₄" "₅" "₆" "₇" "₈" "₉"
-set -g digit_super "¹" "²" "³" "⁴" "⁵" "⁶" "⁷" "⁸" "⁹"
+set -g fish_color_git_staged yellow
+set -g fish_color_git_conflicted red
+set -g fish_color_git_changed blue
+set -g fish_color_git_untracked $fish_color_normal
 
-set -gx fish_color_git_staged yellow
-set -gx fish_color_git_conflicts red
-set -gx fish_color_git_changed blue
-set -gx fish_color_git_untracked $fish_color_normal
+set -g fish_prompt_git_remote_ahead_of "↑"
+set -g fish_prompt_git_remote_behind  "↓"
 
-set -gx fish_prompt_git_remote_ahead_of "↑"
-set -gx fish_prompt_git_remote_behind  "↓"
+set -g fish_prompt_git_status_staged "●"
+set -g fish_prompt_git_status_conflicted '✖'
+set -g fish_prompt_git_status_changed '✚'
+set -g fish_prompt_git_status_untracked "…"
+set -g fish_prompt_git_status_clean "✔"
 
-set -gx fish_prompt_git_status_staged "●"
-set -gx fish_prompt_git_status_conflicts '✖'
-set -gx fish_prompt_git_status_changed '✚'
-set -gx fish_prompt_git_status_untracked "…"
-set -gx fish_prompt_git_status_clean "✔"
-
-set -gx fish_prompt_git_status_order staged conflicts changed untracked
+set -g fish_prompt_git_status_order staged conflicted changed untracked
 
 function __informative_git_prompt --description 'Write out the git prompt'
 
@@ -34,55 +31,94 @@ function __informative_git_prompt --description 'Write out the git prompt'
         return
     end
 
+    set -l branch (git symbolic-ref -q HEAD | cut -c 12-)
+
+    set -l git_branch_info (___fish_git_print_branch_info $branch)
+    set -l git_status_info (___fish_git_print_status_info)
+    set -l git_remote_info (___fish_git_print_remote_info $branch)
+
+    printf "($git_branch_info$git_remote_info|$git_status_info)"
+
+end
+
+function ___fish_git_print_branch_info
+
+    set -l color_branch (set_color -o $fish_color_git_branch)
+    set -l color_normal (set_color $fish_color_normal)
+
+    set -l branch $argv[1]
+    set -l remote_info
+
+    if test -z $branch
+        set -l hash (git rev-parse --short HEAD | cut -c 2-)
+        set branch ":"$hash
+    end
+
+    echo "$color_branch$branch$color_normal"
+
+end
+
+function ___fish_git_print_status_info
+
+    set -l color_normal (set_color $fish_color_normal)
+    set -l color_git_clean (set_color -o $fish_color_git_clean)
+
     set -l changedFiles (git diff --name-status | cut -c 1-2)
     set -l stagedFiles (git diff --staged --name-status | cut -c 1-2)
 
     set -l changed (math (count $changedFiles) - (count (echo $changedFiles | grep "U")))
-    set -l conflicts (count (echo $stagedFiles | grep "U"))
-    set -l staged (math (count $stagedFiles) - $conflicts)
+    set -l conflicted (count (echo $stagedFiles | grep "U"))
+    set -l staged (math (count $stagedFiles) - $conflicted)
     set -l untracked (count (git ls-files --others --exclude-standard))
 
-    set -l branch (git symbolic-ref -q HEAD | cut -c 12-)
-
-    echo -n " ("
-    set_color -o $fish_color_git_branch
-
-    if test -z $branch
-        set hash (git rev-parse --short HEAD | cut -c 2-)
-        echo -n ":"$hash
+    if [ (math $changed + $conflicted + $staged + $untracked) = 0 ]
+        set git_status $color_git_clean$fish_prompt_git_status_clean$color_normal
     else
-        echo -n $branch
-        ___print_remote_info $branch
-    end
-
-    set_color $fish_color_normal
-    echo -n '|'
-
-    if [ (math $changed + $conflicts + $staged + $untracked) = 0 ]
-        set_color -o $fish_color_git_clean
-        echo -n $fish_prompt_git_status_clean
-        set_color $fish_color_normal
-    end
-
-    for i in $fish_prompt_git_status_order
-        if [ $$i != "0" ]
-            set -l color_name fish_color_git_$i
-            set -l status_name fish_prompt_git_status_$i
-            set_color $$color_name
-            echo -n $$status_name$$i
+        for i in $fish_prompt_git_status_order
+            if [ $$i != "0" ]
+                set -l color_name fish_color_git_$i
+                set -l status_name fish_prompt_git_status_$i
+                set -l color (set_color $$color_name)
+                set -l info $$status_name$$i
+                set git_status "$git_status$color$info"
+            end
         end
     end
 
-    set_color $fish_color_normal
-
-    echo -n ")"
+    echo $git_status$color_normal
 
 end
 
-function ___print_remote_info
+function ___fish_git_print_remote_info
 
- set -l branch $argv[1]
- set -l remote_name  (git config branch.$branch.remote)
+    set color_remote (set_color -o $fish_color_git_remote)
+    set color_normal (set_color $fish_color_normal)
+
+    set -l branch $argv[1]
+    set -l remote (____fish_git_remote_info $branch)
+    set -l ahead $remote[1]
+    set -l behind $remote[2]
+    set -l remote_info
+
+
+    if [ $ahead != "0" ]
+        set remote_info $color_remote$fish_prompt_git_remote_ahead_of$color_normal$ahead
+    end
+
+    if [ $behind != "0" ]
+        set remote_info $remote_info$color_remote$fish_prompt_git_remote_behind$color_normal$behind
+    end
+
+    if test -n $remote_info
+        echo " $remote_info"
+    end
+
+end
+
+function ____fish_git_remote_info
+
+    set -l branch $argv[1]
+    set -l remote_name  (git config branch.$branch.remote)
 
     if test -n "$remote_name"
         set merge_name (git config branch.$branch.merge)
@@ -99,7 +135,7 @@ function ___print_remote_info
         set remote_ref "refs/remotes/$remote_name/$merge_name_short"
     end
 
-    set rev_git (eval "git rev-list --left-right $remote_ref...HEAD" ^/dev/null)
+    set -l rev_git (eval "git rev-list --left-right $remote_ref...HEAD" ^/dev/null)
     if test $status != "0"
         set rev_git (git rev-list --left-right $merge_name...HEAD)
     end
@@ -114,22 +150,7 @@ function ___print_remote_info
     set -l ahead (count $isAhead)
     set -l behind (math $remote_diff - $ahead)
 
-    if [ $remote_diff != "0" ]
-        echo -n " "
-    end
-
-    if [ $ahead != "0" ]
-        set_color $fish_color_git_remote
-        echo -n $fish_prompt_git_remote_ahead_of
-        set_color $fish_color_normal
-        echo -n $digit_super[$ahead]
-    end
-#◦
-    if [ $behind != "0" ]
-        set_color $fish_color_git_remote
-        echo -n $fish_prompt_git_remote_behind
-        set_color $fish_color_normal
-        echo -n $digit_sub[$behind]
-    end
+    echo $ahead
+    echo $behind
 
 end
